8dcaf7f21251533df56605d7e34c7fe2bfae5e7a
MarcusCaue <cauemarcuscauefb@gmail.com>
2021-12-24 10:05:24 -0300

    Alterando o nome da pasta

---
+++ estrutura-sequencial/cem-exercicios-cursoemvideo/ex001/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Crie um programa que escreva "Olá, Mundo!" na tela
+ * @data: 24/12/2021
+*/
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("Olá, Mundo!");
+	}
+}
